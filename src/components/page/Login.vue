b055fedd6c380d8ab69a10d35b57af7290dd0b37
吴文龙 <1016674854@qq.com>
2020-12-08 14:02:43 +0800

    基本功能

--- src/components/page/Login.vue
+++ src/components/page/Login.vue
@@ -64,7 +64,7 @@ export default {
     position: relative;
     width: 100%;
     height: 100%;
-    background-image: url(../../assets/img/login-bg.jpg);
+    background-color: dimgray;
     background-size: 100%;
 }
 .ms-title {
